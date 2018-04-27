22f5f8eeea1cee24409b682252412971665c8659
Kai Yan <kyan8@uci.edu>
2018-04-26 20:22:34 -0700

    remove mg add interpolation curve for paddle

---
+++ Assets/Scripts/moveupdown.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveupdown : MonoBehaviour {
+
+
+    public AnimationCurve myCurve;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector2(transform.position.x, myCurve.Evaluate((Time.time % myCurve.length)));
+    }
+} 
